716da19fe14a4fdbafc5f0019d5454638b00a338
Samir Mejia <samir4mt@gmail.com>
2018-05-19 22:33:01 -0500

    Implementacion de logica para multiples idiomas

--- apps/page/urls.py
+++ apps/page/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from apps.page.views import home
 
 urlpatterns = [
+    url(r'^(?P<idiom>[a-z]{2})/$', home, name="home"),
     url(r'^$', home, name="home"),
     # url(r'^registrar', RegistroUsuario.as_view(), name="registrar"),
 
--- apps/page/views.py
+++ apps/page/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
-def home(request):
-	return render(request, 'page/home.html')
+from apps.page.models import Idiom
+
+def home(request, idiom="es"):
+	idioms = Idiom.objects.all()
+	idiom = Idiom.objects.filter(min_name=idiom).first()
+
+	if idiom:
+		context = {"language" : idiom, "idioms":idioms}
+		# return HttpResponse(idiom.min_name);
+		return render(request, 'page/home.html', context)
+	else:
+		return HttpResponse("El idioma no fue encontrado");
