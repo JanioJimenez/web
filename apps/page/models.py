aff8c649573f92615c31fa2c0934163f2c2083d1
Fabian Martinez <fabianmartinezmontenegro@gmail.com>
2018-05-20 01:27:32 -0500

    Se corrigieron los modelos de page y el template home

--- apps/page/models.py
+++ apps/page/models.py
@@ -8,13 +8,17 @@ class Idiom(models.Model):
 	min_name = models.CharField(max_length=2)
 	image = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.name
+
+
 class Info(models.Model):
 	idiom = models.OneToOneField(Idiom, on_delete=models.CASCADE, primary_key=True)
-	title = models.CharField(max_length=20)
-	full_title = models.CharField(max_length=70)
-	greeting = models.BinaryField()
+	name = models.CharField(max_length=70)
+	home = models.CharField(max_length=20)
+	greeting = models.CharField(max_length=50)
 	min_info = models.CharField(max_length=255)
-	description = models.BinaryField()
+	description = models.TextField()
     
 	sign_up = models.CharField(max_length=30)
 	login = models.CharField(max_length=30)
@@ -24,31 +28,40 @@ class Info(models.Model):
 	logout = models.CharField(max_length=30)
 	my_codes = models.CharField(max_length=30)
 
-	charts = models.CharField(max_length=30)
-	chart1_title = models.CharField(max_length=30)
-	chart2_title = models.CharField(max_length=30)
-	chart3_title = models.CharField(max_length=30, default="No hay titulo")
-	chart4_title = models.CharField(max_length=30, default="No hay titulo")
+	charts = models.CharField(max_length=50)
+	chart1_title = models.CharField(max_length=80)
+	chart2_title = models.CharField(max_length=80)
+	chart3_title = models.CharField(max_length=80, default="No hay titulo")
+	chart4_title = models.CharField(max_length=80, default="No hay titulo")
+
+	def __str__(self):
+		return 'Info in {}'.format(self.idiom.name)
 
 
 class Community(models.Model):
 	idiom = models.OneToOneField(Idiom, on_delete=models.CASCADE, primary_key=True)
-	greeting = models.CharField(max_length=30)
-	info = models.BinaryField()
+	greeting = models.CharField(max_length=50)
+	info = models.TextField()
 	btn_try = models.CharField(max_length=10)
 	complete_registration = models.CharField(max_length=50)
 	message_complete_registration = models.CharField(max_length=255)
-	save_data = models.TextField(max_length=20)
+	save_data = models.CharField(max_length=20)
+
+	def __str__(self):
+		return 'Community in {}'.format(self.idiom.name)
 
 class Compiler(models.Model):
 	idiom = models.OneToOneField(Idiom, on_delete=models.CASCADE, primary_key=True)
-	info = models.BinaryField()
+	info = models.TextField()
 	run = models.CharField(max_length=20)
 	save_pc = models.CharField(max_length=30)
 	save_db = models.CharField(max_length=30)
-	log_compilation = models.CharField(max_length=40)
-	result_execution = models.CharField(max_length=40)
+	compilation_log = models.CharField(max_length=40)
+	execution_result = models.CharField(max_length=40)
 	go = models.CharField(max_length=30)
+
+	def __str__(self):
+		return 'Compiler in {}'.format(self.idiom.name)
 
 class Profile(models.Model):
 	idiom = models.OneToOneField(Idiom, on_delete=models.CASCADE, primary_key=True)
@@ -60,12 +73,15 @@ class Profile(models.Model):
 	city = models.CharField(max_length=30)
 	country = models.CharField(max_length=30)
 	language_preference = models.CharField(max_length=40)
-	options_account = models.CharField(max_length=30)
-	password_change = models.CharField(max_length=30)
+	account_options = models.CharField(max_length=30)
+	change_password = models.CharField(max_length=30)
 	change = models.CharField(max_length=30)
 	delete_account = models.CharField(max_length=30)
 	delete = models.CharField(max_length=30)
 	update_data = models.CharField(max_length=30)
 	cancel = models.CharField(max_length=30)
-	save = models.CharField(max_length=30)
+	btn_save = models.CharField(max_length=30)
 	confirmation_message = models.CharField(max_length=255)
+
+	def __str__(self):
+		return 'Profile in {}'.format(self.idiom.name)
--- apps/page/views.py
+++ apps/page/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
-from apps.page.models import Idiom
+from apps.page.models import Idiom, Info
 
 def home(request, idiom="es"):
-	idioms = Idiom.objects.all()
 	idiom = Idiom.objects.filter(min_name=idiom).first()
 
 	if idiom:
+		idioms = Idiom.objects.all()
+
 		context = {"language" : idiom, "idioms":idioms}
-		# return HttpResponse(idiom.min_name);
+		# return HttpResponse(idiom.info.title);
 		return render(request, 'page/home.html', context)
 	else:
 		return HttpResponse("El idioma no fue encontrado");
